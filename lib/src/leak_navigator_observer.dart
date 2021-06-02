9561d6b7d8bdbe6777b78610182ab54508583e18
liujiakuo <liujk@rd.netease.com>
2021-06-02 20:11:57 +0800

    update LICENSE

--- lib/src/leak_navigator_observer.dart
+++ lib/src/leak_navigator_observer.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2021, Jiakuo Liu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/widgets.dart';
 
 import '../leak_detector.dart';
